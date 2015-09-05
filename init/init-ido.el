02bed47460194b28159ad79836c97cc1523f993b
Bartosz <ethifus@gmail.com>
2015-09-05 13:59:47 +0200

    Add smex package.

--- init/init-ido.el
+++ init/init-ido.el
@@ -1,22 +1,35 @@
 ;; Use ido-mode
-(require 'ido)
-(use-package flx-ido
-  :ensure t
+
+(use-package ido
   :init
-  (ido-mode t)
-  (flx-ido-mode t)
   (setq ido-create-new-buffer 'always
         ido-enable-flex-matching t
         ido-everywhere t
-        ido-use-filename-at-point nil
+        ido-use-filename-at-point 'guess
         ido-case-fold nil
         ido-use-faces nil)
-  (global-set-key
-   "\M-x"
-   (lambda ()
-     (interactive)
-     (call-interactively
-      (intern
-       (ido-completing-read
-        "M-x "
-        (all-completions "" obarray 'commandp)))))))
+  (ido-mode t)
+  ;; (global-set-key
+  ;;  "\M-x"
+  ;;  (lambda ()
+  ;;    (interactive)
+  ;;    (call-interactively
+  ;;     (intern
+  ;;      (ido-completing-read
+  ;;       "M-x "
+  ;;       (all-completions "" obarray 'commandp))))))
+  )
+
+(use-package flx-ido
+  :ensure t
+  :init
+  (flx-ido-mode t))
+
+;; Show recently and mostly used commands on M-x
+(use-package smex
+  :ensure t
+  :config
+  (smex-initialize)
+  :init
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex))
