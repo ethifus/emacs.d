c9b90c41e9661a361379164f28eae14e0a2c58df
Bartosz <ethifus@gmail.com>
2016-09-10 20:05:45 +0200

    Improve code to switch between light and dark color theme.

--- init/init-themes.el
+++ init/init-themes.el
@@ -1,21 +1,46 @@
 ;; Configure color themes
 
 (use-package color-theme-sanityinc-tomorrow
+  :defer t
   :ensure t)
 
-(defun dark ()
-  "Activate default dark theme (monokai)."
-  (interactive)
-  (use-package monokai-theme :ensure t)
-  (disable-theme 'leuven)
-  (load-theme 'monokai t))
+(use-package monokai-theme
+  :defer t
+  :ensure t)
+
+(use-package leuven-theme
+  :defer t
+  :ensure t)
+
+(defgroup light-dark-themes nil
+  "Pair of light and dark color theme names used to switch with
+ `light' and `dark' command.")
+
+(defcustom light-theme-name "leuven"
+  "Light color theme name."
+  :group 'light-dark-themes
+  :type '(string))
+
+(defcustom dark-theme-name "monokai"
+  "Dark color theme name."
+  :group 'light-dark-themes
+  :type '(string))
+
+(defun enable-single-theme (theme)
+  (mapcar 'disable-theme custom-enabled-themes)
+  (let ((theme-symbol (intern theme)))
+    (load-theme theme-symbol)
+    (enable-theme theme-symbol)))
 
 (defun light ()
-  "Activate default light theme (leuven)."
+  "Activate light theme from `light-theme-name' variable."
   (interactive)
-  (use-package leuven-theme :ensure t)
-  (disable-theme 'monokai)
-  (load-theme 'leuven t))
+  (enable-single-theme light-theme-name))
+
+(defun dark ()
+  "Activate dark theme from `light-theme-name' variable."
+  (interactive)
+  (enable-single-theme dark-theme-name))
 
 
 (provide 'init-themes)
