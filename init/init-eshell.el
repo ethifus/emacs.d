4b2615403314f3b640f0479b3692c9d578860682
Bartosz <ethifus@gmail.com>
2017-11-21 21:16:40 +0100

    Modify some keys in eshell mode.

--- init/init-eshell.el
+++ init/init-eshell.el
@@ -5,7 +5,11 @@
   (setq eshell-where-to-jump 'begin
         eshell-review-quick-commands nil
         eshell-smart-space-goes-to-end t)
-  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on))
+  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+  (add-hook 'eshell-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<up>") (previous-line))
+            (local-set-key (kbd "<down>") (next-line)))))
 
 
 (provide 'init-eshell)
