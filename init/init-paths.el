b19566e5de37c07dfbe05ce411295762515f3907
Bartosz <ethifus@gmail.com>
2018-01-14 16:39:37 +0100

    Updated path settings in init-paths.el.

--- init/init-paths.el
+++ init/init-paths.el
@@ -1,8 +1,15 @@
-;; Add local paths to PATH enviroment.
-(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/.local/bin")))
+(defun eth/init-paths (paths)
+  "Add paths to PATH environment and to binary locations in
+`exec-path'."
+  (let ((paths (mapcar 'expand-file-name paths)))
+    (setenv "PATH"
+            (apply 'concat
+                   (append (list (getenv "PATH"))
+                           (mapcar (lambda (element) (concat ":" element)) paths))))
+    (setq exec-path (append exec-path paths))))
 
-;; Add local binary locations.
-(setq exec-path (append exec-path (list (expand-file-name "~/.local/bin"))))
+(eth/init-paths (list "~/.local/bin" "~/.local/lib/node_modules/bin"))
+
 
 ;; Add path to custom modules and init scripts.
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
